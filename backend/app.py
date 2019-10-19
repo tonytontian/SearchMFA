a7ca18aab255ead3fe8f2a4ee6837500ca3e0fda
Hsiang-wei Chao <1155032067@link.cuhk.edu.hk>
2019-10-19 17:39:54 -0400

    checkpoint

--- backend/app.py
+++ backend/app.py
@@ -4,18 +4,31 @@ from flask_cors import CORS, cross_origin
 from werkzeug.utils import secure_filename
 from pathlib import Path
 from PIL import Image
+from bitarray import bitarray
 
-UPLOAD_FOLDER = './images'
+from init import init
 
+# parameters
+STORAGE=Path('storage')
+HASH = 'dhash'
+
+# flask config
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# init
+records, hashes, hash_func = init(STORAGE, HASH)
 
+# upload
 @app.route('/upload', methods=['POST'])
 @cross_origin(origin='*')
 def upload_file():
     file = request.files.get('file')
     image = Image.open(io.BytesIO(file.read()))
+    hash_str = ''.join(map(str, hash_func(image).hash.flatten().astype(int)))
+    bita = bitarray(hash_str)
+    # get most similar image
+    hamming_dist = [(bita ^ bitb).count(1) for bitb in hashes]
     
     return ''
 
---
+++ backend/init.py
@@ -0,0 +1,25 @@
+import json
+import numpy as np
+from bitarray import bitarray
+import imagehash
+
+hash_functions = {
+    'aHash': imagehash.average_hash,
+    'pHash': imagehash.phash,
+    'dHash': imagehash.dhash,
+    'wHash': imagehash.whash,
+}
+
+
+def get_hashes(name):
+    hashes = [bitarray(record[name]) for record in records]
+    return hashes
+
+def init(STORAGE, HASH):
+    records_json = STORAGE / 'records.json'
+    with open(records_json) as f:
+        records = json.load(f)
+    hashes = get_hashes(HASH)
+    hash_func = hash_functions[HASH]
+    
+    return records, hashes, hash_func
