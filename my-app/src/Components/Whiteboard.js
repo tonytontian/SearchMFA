f2b7554443203488d793aac23aa6d5bf7c2fc93c
Hsiang-wei Chao <1155032067@link.cuhk.edu.hk>
2019-10-20 19:57:45 -0400

    mobile config friendly

--- my-app/src/Components/Whiteboard.js
+++ my-app/src/Components/Whiteboard.js
@@ -15,7 +15,7 @@ export default class className extends Component {
         super(props);
         this.state = {
             uploadedFile:Logo,
-            src : 'http://localhost:5000/images/1',
+            src : `http://${window.location.hostname}:5000/images/1`,
             detail : "Founded in 1870, the Museum of Fine Arts, Boston, stands on the historic homelands of the Massachusett people, a site which has long served as a place of meeting and exchange among different nations. The MFA opened its doors to the public on July 4, 1876, the nation's centennial. Built in Copley Square, the MFA was then home to 5,600 works of art. Over the next several years, the collection and number of visitors grew exponentially, and in 1909 the Museum moved to its current home on Huntington Avenue.\n" +
                     "\n" +
                     "Today the MFA is one of the most comprehensive art museums in the world; the collection encompasses nearly 500,000 works of art. We welcome more than one million visitors each year to experience art from ancient Egyptian to contemporary, special exhibitions, and innovative educational programs.\n" +
@@ -45,7 +45,7 @@ export default class className extends Component {
 
     setFetchedFile(id, detail ){
         this.setState({
-            src: `http://localhost:5000/images/${id}`,
+            src: `http://${window.location.hostname}:5000/images/${id}`,
             detail:detail
         })
     }
