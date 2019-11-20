6e3b471e191587ea8d9d4b4e24b312320120297e
Hsiangwei Chao <1155032067@link.cuhk.edu.hk>
2019-11-20 17:49:21 -0500

    update analyze2 list

--- analysis2/resize_image.py
+++ analysis2/resize_image.py
@@ -2,8 +2,10 @@ from pathlib import Path
 from PIL import Image
 import tqdm
 
-image_dir = Path('dataset/testing dataset')
-target_image_dir = Path('dataset/testing dataset small')
+# image_dir = Path('dataset/testing dataset')
+# target_image_dir = Path('dataset/testing dataset small')
+image_dir = Path('../analysis1/dataset/taken')
+target_image_dir = Path('../analysis1/dataset/taken_small')
 target_image_dir.mkdir(exist_ok=True)
 
 image_paths = list(image_dir.glob(r'*.JPG'))
@@ -11,7 +13,10 @@ image_paths = list(image_dir.glob(r'*.JPG'))
 def resize_image(image_path):
     image = Image.open(image_path)
     w, h = image.size
-    image_resized = image.resize((w//4, h//4))
+    if not (w>2000 or h>2000):
+        image_resized = image
+    else:
+        image_resized = image.resize((w//4, h//4))
     #
     new_path = target_image_dir / image_path.name
     image_resized.save(new_path)
